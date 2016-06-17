47ef25b38201132487e146f78a0ff802ea10c7e8
Antanas Majus <antanas.majus@gmail.com>
2016-06-17 11:01:23 +0200

    Add initial page

---
+++ assets/js/functions.js
@@ -0,0 +1,51 @@
+jQuery(document).ready(function($) {
+	"use strict";
+
+
+    var windowheight = jQuery(window).height();
+    if(windowheight > 650)
+    {
+         $('.pattern').removeClass('height-resize');
+    }
+  
+    $('#about').backstretch([
+       "images/ceremonija-min.jpg", 
+       "images/ceremonija_1-min.jpg",
+       "images/ceremonija_2-min.jpg",
+        ],  {duration: 4000, fade: 1000, alignX: 'center'});
+
+    $('#subscribe').backstretch([
+       "images/krantas_1-min.jpg", 
+       "images/krantas_2-min.jpg",
+       "images/krantas_3-min.jpg",
+        ],  {duration: 4000, fade: 1000, alignX: 'center'});
+    
+    $('#contact').backstretch([
+       "images/kontaktai-min.jpg", 
+        ],  {alignX: 'center'});
+
+	$('#time_countdown').countDown({  
+        targetDate: {
+            'day': 10,
+            'month': 9,
+            'year': 2016,
+            'hour': 13,
+            'min': 45,
+            'sec': 0
+        }, omitWeeks: true
+	    });
+
+	$('.next-section .go-to-about').click(function() {
+    	$('html,body').animate({scrollTop:$('#about').offset().top}, 1000);
+  	});
+  	$('.next-section .go-to-subscribe').click(function() {
+    	$('html,body').animate({scrollTop:$('#subscribe').offset().top}, 1000);
+  	});
+  	$('.next-section .go-to-contact').click(function() {
+    	$('html,body').animate({scrollTop:$('#contact').offset().top}, 1000);
+  	});
+  	$('.next-section .go-to-page-top').click(function() {
+    	$('html,body').animate({scrollTop:$('#page-top').offset().top}, 1000);
+  	});
+});
+
